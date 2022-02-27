ab74467fcea59e55e5e45fbe00312730d97bdf33
sanskar-pawar <sanskar.pawar@coditas.com>
2022-02-28 02:09:49 +0530

    added data bindings

--- app/src/main/java/com/example/androidarchitecturejetpack/MainActivity.kt
+++ app/src/main/java/com/example/androidarchitecturejetpack/MainActivity.kt
@@ -1,31 +1,36 @@
 package com.example.androidarchitecturejetpack
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.*
+import com.example.androidarchitecturejetpack.databinding.ActivityMainBinding
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
    lateinit var mainViewModel: MainViewModel
    lateinit var textForCount : TextView
     lateinit var textForLiveData : TextView
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main ) //created object of data binding to access the views
+        //setContentView(R.layout.activity_main)
         mainViewModel = ViewModelProvider(this,MainViewModelFactory(10)).get(MainViewModel::class.java)
         //provides view model, passing lifecycle
         //TO make an object we used ViewModelProvider and passed lifecycle owner(this) and which class object we need
         //2ed parameter is  fixed value that we want pass to the view model using viewModelFactory
 
-        val buttonClick :Button=findViewById(R.id.buttonForCount)
-         textForCount=findViewById(R.id.txtForCount)
-        textForLiveData=findViewById(R.id.txtViewForLiveData)
-        buttonClick.setOnClickListener {
+
+
+
+        binding.buttonForCount.setOnClickListener {
             mainViewModel.Increment()
 
             mainViewModel.updateLiveData() // to call the method
@@ -46,7 +51,7 @@ class MainActivity : AppCompatActivity() {
 
             //whenever the livedata will change the this code will get executed..
 
-            textForLiveData.text = it //update the value with string of mutable live data
+            binding.txtViewForLiveData.text = it //update the value with string of mutable live data
 
 
         })
@@ -59,7 +64,7 @@ class MainActivity : AppCompatActivity() {
 
 
     fun setText() {
-        textForCount.text=mainViewModel.count.toString()//to set the count to textView
+        binding.txtForCount.text=mainViewModel.count.toString()//to set the count to textView
     }
 
     override fun onResume() {
--- app/src/main/java/com/example/androidarchitecturejetpack/MainViewModel.kt
+++ app/src/main/java/com/example/androidarchitecturejetpack/MainViewModel.kt
@@ -1,10 +1,11 @@
 package com.example.androidarchitecturejetpack
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel(InitialValue:Int):ViewModel() {     //InitialValue is the value that we passed in
-                                                            //viewmodelFactoryclass to set initalvalue of counter to 10
+                                                        //viewmodelFactoryclass to set initalvalue of counter to 10
     var count : Int =InitialValue
 
    fun Increment()//method which we are calling in MAinActivity
@@ -13,10 +14,14 @@ class MainViewModel(InitialValue:Int):ViewModel() {     //InitialValue is the va
    }
 
 
-    val factsLiveData = MutableLiveData<String>("This id a fact") // this is utable live data (we can change
-                                                                        // the data)
+   private val factsLiveDataObject = MutableLiveData("This id a fact") // this is update live data (we can change
+                                                                                // the data)
+    val factsLiveData : LiveData<String> // just normal Livedata implementation
+    get() = factsLiveDataObject // getting factsLiveDataObject here ( does not change output)
+
     fun updateLiveData(){
-        factsLiveData.value = "Another fact" // this will update the live data
+    factsLiveDataObject.value = "Another fact" // this will update the live data
+
     }
 
 
