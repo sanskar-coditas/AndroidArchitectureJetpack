63347c301cbf76e8c98b0e13a8a12d8fe1b9566a
sanskar-pawar <sanskar.pawar@coditas.com>
2022-02-28 01:12:27 +0530

    added mutable Live data class in view model and observed it in main activity

--- app/src/main/java/com/example/androidarchitecturejetpack/MainActivity.kt
+++ app/src/main/java/com/example/androidarchitecturejetpack/MainActivity.kt
@@ -12,32 +12,49 @@ import org.w3c.dom.Text
 class MainActivity : AppCompatActivity() {
    lateinit var mainViewModel: MainViewModel
    lateinit var textForCount : TextView
+    lateinit var textForLiveData : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mainViewModel = ViewModelProvider(this,MainViewModelFactory(10)).get(MainViewModel::class.java)//provides view model, passing lifecycle
+        mainViewModel = ViewModelProvider(this,MainViewModelFactory(10)).get(MainViewModel::class.java)
+        //provides view model, passing lifecycle
         //TO make an object we used ViewModelProvider and passed lifecycle owner(this) and which class object we need
         //2ed parameter is  fixed value that we want pass to the view model using viewModelFactory
 
         val buttonClick :Button=findViewById(R.id.buttonForCount)
          textForCount=findViewById(R.id.txtForCount)
-
+        textForLiveData=findViewById(R.id.txtViewForLiveData)
         buttonClick.setOnClickListener {
             mainViewModel.Increment()
+
+            mainViewModel.updateLiveData() // to call the method
+
            setText()
 
         }
    setText()
 
 
-
-
-
-
-
         lifecycle.addObserver(Obeserver())
         Log.d("Main","ACTIVITY OnCreate")
+
+
+
+        mainViewModel.factsLiveData.observe(this, Observer {
+            // it is string because factsLivedata is string
+
+            //whenever the livedata will change the this code will get executed..
+
+            textForLiveData.text = it //update the value with string of mutable live data
+
+
+        })
+    //1st parameter is lifecycle owner on the basis of
+    //that it will decide data send or not. it only sends data
+    // it its in active state.
+
+
     }
 
 
--- app/src/main/java/com/example/androidarchitecturejetpack/MainViewModel.kt
+++ app/src/main/java/com/example/androidarchitecturejetpack/MainViewModel.kt
@@ -1,14 +1,23 @@
 package com.example.androidarchitecturejetpack
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel(val InitialValue:Int):ViewModel() {     //InitialValue is the value that we passed in
+class MainViewModel(InitialValue:Int):ViewModel() {     //InitialValue is the value that we passed in
                                                             //viewmodelFactoryclass to set initalvalue of counter to 10
-
     var count : Int =InitialValue
 
    fun Increment()//method which we are calling in MAinActivity
    {
        count++
    }
+
+
+    val factsLiveData = MutableLiveData<String>("This id a fact") // this is utable live data (we can change
+                                                                        // the data)
+    fun updateLiveData(){
+        factsLiveData.value = "Another fact" // this will update the live data
+    }
+
+
 }
